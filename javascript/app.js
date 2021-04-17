bed67b3f7ef90c5cc4d1efddff0c2d3087512585
rodrigovilcamontesinos <63301748+rodrigovilcamontesinos@users.noreply.github.com>
2021-04-16 20:38:09 -0500

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -2,7 +2,19 @@
 // Returns a sorted array
 function insertion_sort(numbers) {
 
-    //Write your code here
+    for(let i = 1; i < numbers.length; i++){
+        for(let j = i; j > 0; j--){
+            if(numbers[j] < numbers[j-1]){
+                const a = numbers[j];
+                numbers[j] = numbers[j-1];
+                numbers[j-1] = a;
+            }
+            else{
+                break;
+            }
+        }
+    }
+    return [-1];
 
     return [-1];
 }
@@ -21,9 +33,11 @@ function factorial(number) {
 // Returns a single integer
 function fibonacci(number) {
 
-    //Write your code here
-
-    return -1
+    let serie = [1, 1];
+    for (let i = 2; i < number; i++) {
+        serie[i] = serie[i - 1] + serie[i - 2];
+    }
+    return serie[number - 1]
 }
 
 // Param expression: string containing a mathematical expression
