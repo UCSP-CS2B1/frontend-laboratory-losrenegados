fc36c247b078d47c5460f102a485cd84f8075b44
walalo1 <guillenmarco323@gmail.com>
2021-04-17 08:41:19 -0500

    Factorial commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,48 +1,33 @@
 // Param numbers: array of numbers
 // Returns a sorted array
-function insertion_sort(numbers) {
-
-    
-
-    return [-1];
-}
-
+function insertion_sort(numbers) { return [ -1 ]; }
 
 // Param number: single integer
 // Returns a single integer
 function factorial(number) {
-
-    //Write your code here
-
-    return -1;
+  var i = 1;
+  var j;
+  for (j = number; j > 0; j--) {
+    i = i * j;
+  }
+  return i;
 }
 
 // Param number: single integer
 // Returns a single integer
 function fibonacci(number) {
 
-    let serie = [1, 1];
-    for (let i = 2; i < number; i++) {
-        serie[i] = serie[i - 1] + serie[i - 2];
-    }
-    return serie[number - 1]
+  let serie = [ 1, 1 ];
+  for (let i = 2; i < number; i++) {
+    serie[i] = serie[i - 1] + serie[i - 2];
+  }
+  return serie[number - 1]
 }
 
 // Param expression: string containing a mathematical expression
 // Returns an integer value
-function evaluation(expression) {
+function evaluation(expression) { return expression; }
 
-    
-
-    return expression;
-}
-
-
-if(typeof process === 'object') {
-    module.exports =  {
-        insertion_sort,
-        factorial,
-        fibonacci,
-        evaluation
-    };
+if (typeof process === 'object') {
+  module.exports = {insertion_sort, factorial, fibonacci, evaluation};
 }
